0724f7ca833c35702d3667a1b3f5c5c125379a18
Tim Newbold <timnewbold.research@gmail.com>
2021-09-29 15:58:26 +0100

    Added calculation of species richness estimators to SiteMetrics function

--- predictsFunctions/R/SiteMetrics.R
+++ predictsFunctions/R/SiteMetrics.R
@@ -66,10 +66,21 @@ function(diversity, extra.cols=NULL) {
                                            site.occurrence, 
                                            site.species.richness)
   
+  # Calculate specified species richness estimators
+  if ("Chao" %in% srEstimators){
+    chao <- .SiteChao(diversity, site.abundance)
+  }
+  
+  if ("Rare" %in% srEstimators){
+    rsrich<-.SiteRarefiedRichness(diversity,site.abundance)
+  }
+  
   cat('Assembling site-level values\n')
   res <- cbind(diversity[!duplicated(diversity$SSS),c(site.cols,'SS','SSS')], 
                Total_abundance=total.abundance, 
-               Species_richness=species.richness)
+               Species_richness=species.richness,
+               ChaoR=if ("Chao" %in% srEstimators) chao else NA,
+               Richness_rarefied=if ("Rare" %in% srEstimators) rsrich else NA)
   rownames(res) <- NULL
   
   res <- res[order(res$Source_ID, res$Study_number, res$Site_number),]
@@ -113,4 +124,97 @@ function(diversity, extra.cols=NULL) {
            sum)
   
   return (sr)
+}
+
+.SiteChao <- function(diversity, site.abundance) {
+  .Log("Computing Chao\n")
+  # Compute Chao only for studies where Diversity_metric_is_suitable_for_Chao 
+  # is TRUE and all measurements within the study are integers.
+  
+  # http://stackoverflow.com/questions/3476782/how-to-check-if-the-number-is-integer
+  # TODO all.equal
+  study.chao.suitable <- 
+    tapply(diversity$Measurement[diversity$Diversity_metric_is_suitable_for_Chao],
+           diversity$SS[diversity$Diversity_metric_is_suitable_for_Chao], 
+           function(m) all(floor(m)==m))
+  
+  # Studies for which Diversity_metric_is_suitable_for_Chao is FALSE will have 
+  # a value of NA in study.chao.suitable
+  study.chao.suitable[is.na(study.chao.suitable)] <- FALSE
+  site.chao.suitable <- study.chao.suitable[tapply(diversity$SS, diversity$SSS, unique)]
+  
+  diversity$Is_Chao_suitable <- site.chao.suitable[diversity$SSS]
+  
+  # Only abundance metrics should be marked as suitable for Chao 
+  stopifnot(!any(site.chao.suitable & !site.abundance))
+  chao <- rep(NA, length(site.abundance))
+  chao[site.chao.suitable] <- 
+    tapply(diversity$Measurement[diversity$Is_Chao_suitable], 
+           droplevels(diversity$SSS[diversity$Is_Chao_suitable]), 
+           function(m) sum(m>0) + (((sum(m==1) * (sum(m==1)-1)) / (2*(sum(m==2)+1)))))
+  return (chao)
+}
+
+.SiteRarefiedRichness<-function(diversity, site.abundance){
+  .Log("Computing Rarefied Species Richness\n")
+  # Compute rarefied richness only for studies where Diversity_metric_is_suitable_for_Chao 
+  # is TRUE and all measurements within the study are integers.
+  
+  # http://stackoverflow.com/questions/3476782/how-to-check-if-the-number-is-integer
+  # TODO all.equal
+  study.rsr.suitable <- 
+    tapply(diversity$Measurement[diversity$Diversity_metric_is_suitable_for_Chao],
+           diversity$SS[diversity$Diversity_metric_is_suitable_for_Chao], 
+           function(m) all(floor(m)==m))
+  
+  # Studies for which Diversity_metric_is_suitable_for_Chao is FALSE will have 
+  # a value of NA in study.rsr.suitable
+  study.rsr.suitable[is.na(study.rsr.suitable)] <- FALSE
+  site.rsr.suitable <- study.rsr.suitable[tapply(diversity$SS, diversity$SSS, unique)]
+  
+  diversity$Is_RSR_suitable <- site.rsr.suitable[diversity$SSS]
+  
+  # Only abundance metrics should be marked as suitable for rarefied richness 
+  stopifnot(!any(site.rsr.suitable & !site.abundance))
+  
+  rsrich <- rep(NA, length(site.abundance))
+  
+  if (any(diversity$Is_RSR_suitable)){
+    siteTotalAbund<-aggregate(Measurement~SSS,data=droplevels(
+      diversity[diversity$Is_RSR_suitable,]),FUN=sum)
+    siteTotalAbund$SS<-diversity$SS[match(siteTotalAbund$SSS,diversity$SSS)]
+    studyMinSiteAbund<-aggregate(Measurement~SS,data=siteTotalAbund,FUN=
+                                   function(x) min(x[x>0]))
+    diversity$studyMinSiteAbund<-studyMinSiteAbund$Measurement[match(
+      diversity$SS,studyMinSiteAbund$SS)]
+    
+    sample.n <- split(diversity$studyMinSiteAbund[diversity$Is_RSR_suitable], 
+                      droplevels(diversity$SSS[diversity$Is_RSR_suitable]))
+    values <- split(diversity$Measurement[diversity$Is_RSR_suitable],
+                    droplevels(diversity$SSS[diversity$Is_RSR_suitable]))
+    
+    rsr<-matrix(nrow=length(which(site.rsr.suitable)),ncol=1000)
+    for (i in 1:1000){
+      rsr[,i]<-mapply(function(vals,samp){
+        sp<-as.character(1:length(vals))
+        ind<-rep(sp,vals)
+        if (length(ind)==0){
+          sr<-0
+        } else {
+          rare.sp<-sample(ind,samp[1])
+          sr<-length(table(rare.sp))
+        }
+        return(sr)
+      },values,sample.n)
+    }
+    
+    rsrich[site.rsr.suitable]<-apply(rsr,1,mean)
+    
+  }
+  
+  
+  
+  
+  return(rsrich)
+  
 }
